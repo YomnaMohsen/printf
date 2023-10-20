8f36183d71fe219df41907d5b2d655795660c508
YomnaMohsen <ymn_mohsen@yahoo.com>
2023-10-20 11:14:45 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -51,43 +51,4 @@ int _printf(const char *format, ...)
 	va_end (ap);
 	return (sum);
 }
-#include <stdio.h>
-#include <unistd.h>
 
-int run_handlers(const char *format, va_list args, buf_t *buf);
-void printbuff_free(buf_t *buf, va_list args);
-int _printf(const char *format, ...);
-/**
-* run_handlers - process string read from _printf
-* @buf: pointer to struct
-* @args: list of args
-* @format: string format
-* Return: int
-*/
-
-int run_handlers(const char *format, va_list args, buf_t *buf)
-{
-	int i, count = 0;
-	int (*f)(va_list, buf_t *);
-
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			f = frmt_specifier(format + i + 1);
-			if (f != NULL)
-			{
-				i++;
-				count += f(args, buf);
-				continue;
-			}
-			else if (format[i + 1] == '\0')
-			{
-				count = -1;
-				break;
-			}
-		}
-		count += cpy_buf(buf, (format + i), 1);
-	}
-	return (count);
-}
--- number.c
+++ number.c
@@ -51,6 +51,7 @@ char *convert(long int num, int base, int flags, params_t *params)
 
 int print_unsigned(va_list ap, params_t *params)
 {
+	
 	unsigned long l;
 
 	if (params->l_modifier)
